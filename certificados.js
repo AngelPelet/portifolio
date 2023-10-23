dda96f4331ee557bbbe263aca7688a83d3b7000b
AngelPelet <angelpeleteiro2001@gmail.com>
2023-10-23 19:17:48 -0300

    att_015

--- certificados.js
+++ certificados.js
@@ -45,4 +45,19 @@ const toggleModal2 = () => {
     el.addEventListener("click", () => toggleModal2())
 })
 
+/*CERTIFICADO 4*/
+const openModalButton3 = document.querySelector(".open-modal3")
+const closeModalButton3 = document.querySelector(".close-modal3")
+const modal3 = document.querySelector(".modal3")
+const fade3 = document.querySelector(".fade3")
+
+const toggleModal3 = () => {
+    modal3.classList.toggle("hide3")
+    fade3.classList.toggle("hide3")
+}
+
+[openModalButton3, closeModalButton3, fade3].forEach(function (el) {
+    el.addEventListener("click", () => toggleModal3())
+})
+
 
